0621f70a457814e44e4b6dc43b2f3322940dcf1b
Dmitriy <freedev39@gmail.com>
2025-03-29 16:18:36 +0300

    added config.py

---
+++ src/config.py
@@ -0,0 +1,17 @@
+
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    yandex_client_id: str
+    yandex_client_secret: str
+    yandex_redirect_uri: str
+
+
+@lru_cache(1)
+def get_settings():
+    return Settings()
